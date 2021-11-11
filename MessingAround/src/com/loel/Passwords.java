66a7a39322a24e5d4ad498a365e237a9bc65c832
NerfedWizard <loelnelson@gmail.com>
2021-11-11 17:29:16 -0600

    Made password generator

---
+++ MessingAround/src/com/loel/Passwords.java
@@ -0,0 +1,42 @@
+package com.loel;
+
+import org.passay.*;
+
+public class Passwords {
+
+	public static void main(String[] args) {
+		System.out.println(generatePassayPassword());
+
+	}
+
+	public static String generatePassayPassword() {
+		PasswordGenerator gen = new PasswordGenerator();
+		CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+		CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+		lowerCaseRule.setNumberOfCharacters(2);
+
+		CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+		CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+		upperCaseRule.setNumberOfCharacters(2);
+
+		CharacterData digitChars = EnglishCharacterData.Digit;
+		CharacterRule digitRule = new CharacterRule(digitChars);
+		digitRule.setNumberOfCharacters(2);
+
+		CharacterData specialChars = new CharacterData() {
+			public String getErrorCode() {
+				return AllowedCharacterRule.ERROR_CODE;
+			}
+
+			public String getCharacters() {
+				return "!@#$%^&*()_+";
+			}
+		};
+		CharacterRule splCharRule = new CharacterRule(specialChars);
+		splCharRule.setNumberOfCharacters(2);
+
+		String password = gen.generatePassword(10, splCharRule, lowerCaseRule, upperCaseRule, digitRule);
+		return password;
+	}
+
+}
